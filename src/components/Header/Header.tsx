1b0a4da0faa2f0dfae41b80461a84053fc8e81be
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-09 18:56:34 +0300

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import logo from '/logo.png';
+import './Header.css';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <img className="header-logo" src={logo} alt="Rick and Morty" />
+        <h1>
+          Rick·and·Morty
+          <br />
+          character·search
+        </h1>
+      </header>
+    );
+  }
+}
