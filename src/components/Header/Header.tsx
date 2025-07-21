83d118b2e7d99d5d7695de47485df74440e139a6
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-21 14:44:29 +0300

    fix: change Header to FC

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import logo from '/logo.png';
 import './Header.css';
 
-export class Header extends Component {
-  render() {
-    return (
-      <header>
-        <img className="header-logo" src={logo} alt="Rick and Morty" />
-        <h1>Rick and Morty character search</h1>
-      </header>
-    );
-  }
+export function Header() {
+  return (
+    <header>
+      <img className="header-logo" src={logo} alt="Rick and Morty" />
+      <h1>Rick and Morty character search</h1>
+    </header>
+  );
 }
