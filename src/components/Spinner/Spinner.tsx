7388c990f35e80e60f5c3e8e7cd09213659f7c35
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-21 14:47:49 +0300

    fix: change Spinner to FC

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,8 +1,5 @@
-import { Component } from 'react';
 import './Spinner.css';
 
-export class Spinner extends Component {
-  render() {
-    return <span className="loader"></span>;
-  }
+export function Spinner() {
+  return <span className="loader"></span>;
 }
