6153345083279ffb70d2fbd90b7039531b118dd5
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-22 14:24:12 +0300

    fix: change SearchResult to FC

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,46 +1,40 @@
 import './SearchResult.css';
-import { Component } from 'react';
-import type { Props, ErrorSearchResult } from '../../services/types';
+import type { Props } from '../../services/types';
 import { Button } from '../Button/Button';
+import { useState } from 'react';
 
-export class SearchResult extends Component<Props> {
-  state: ErrorSearchResult = {
-    hasError: false,
+export function SearchResult({ results }: Props) {
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const sendError = () => {
+    setHasError(true);
   };
 
-  sendError = () => {
-    this.setState({ hasError: true });
-  };
-
-  render() {
-    const { results } = this.props;
-
-    if (results === null) {
-      return null;
-    }
-
-    if (results.length === 0) {
-      return <p>Nothing was found</p>;
-    }
-
-    if (this.state.hasError) {
-      throw new Error('Something broke');
-    }
-
-    return (
-      <>
-        <ul className="result-list">
-          {results.map((char) => (
-            <li className="result-item" key={char.id}>
-              <img className="char-image" src={char.image} alt={char.name} />
-              <span className="char-name">{char.name}</span>
-            </li>
-          ))}
-        </ul>
-        <Button onClick={this.sendError} style={{ marginTop: '20px' }}>
-          Error
-        </Button>
-      </>
-    );
+  if (results === null) {
+    return null;
   }
+
+  if (results.length === 0) {
+    return <p>Nothing was found</p>;
+  }
+
+  if (hasError) {
+    throw new Error('Something broke');
+  }
+
+  return (
+    <>
+      <ul className="result-list">
+        {results.map((char) => (
+          <li className="result-item" key={char.id}>
+            <img className="char-image" src={char.image} alt={char.name} />
+            <span className="char-name">{char.name}</span>
+          </li>
+        ))}
+      </ul>
+      <Button onClick={sendError} style={{ marginTop: '20px' }}>
+        Error
+      </Button>
+    </>
+  );
 }
