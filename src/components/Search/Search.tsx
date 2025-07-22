b07c11b7ca082d36f842ee19dfa296b09409a4d7
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-22 20:29:50 +0300

    fix: change Search to FC

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,41 +1,30 @@
 import './Search.css';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import type { ChangeEvent } from 'react';
 import { Button } from '../Button/Button';
-import type { SearchState } from '../../services/types';
+import type { Character } from '../../services/types';
 import { SearchResult } from '../SearchResult/SearchResult';
 import { Spinner } from '../Spinner/Spinner';
 import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary';
 
-export class Search extends Component {
-  state: SearchState = {
-    name: localStorage.getItem('inputName') || '',
-    results: null,
-    loading: false,
-    error: null,
-  };
+export function Search() {
+  const [charName, setCharName] = useState<string>(
+    localStorage.getItem('inputName') || ''
+  );
+  const [results, setResults] = useState<Character[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  componentDidMount() {
-    if (this.state.name.trim() !== '') {
-      this.sendRequest();
-    }
-  }
+  useEffect(() => {
+    sendRequest();
+  }, []);
 
-  handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
+  const sendRequest = async () => {
+    setLoading(true);
 
-    this.setState({
-      name: value,
-    });
-
-    localStorage.setItem('inputName', value);
-  };
-
-  sendRequest = async () => {
-    this.setState({ loading: true });
     try {
       const response = await fetch(
-        `https://rickandmortyapi.com/api/character/?name=${this.state.name}&page=1`
+        `https://rickandmortyapi.com/api/character/?name=${charName}&page=1`
       );
       const data = await response.json();
 
@@ -43,45 +32,40 @@ export class Search extends Component {
         await new Promise((resolve) => setTimeout(resolve, 3000));
       }
 
-      this.setState({ results: data.results || [] });
+      setResults(data.results || []);
     } catch (error) {
       console.error('Error receive:', error);
-      this.setState({
-        results: [],
-        error: 'Error receive: ' + (error as Error).message,
-      });
+      setResults([]);
+      setError('Error receive: ' + (error as Error).message);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  render() {
-    return (
-      <section className="section-search">
-        <h2>Search</h2>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleNameChange}
-            className="search-input"
-          />
-
-          <Button onClick={this.sendRequest}>Search</Button>
-        </form>
-        <h2>Results</h2>
-        {this.state.error && (
-          <p className="error-message">{this.state.error}</p>
-        )}
-
-        <ErrorBoundary>
-          {this.state.loading ? (
-            <Spinner />
-          ) : (
-            <SearchResult results={this.state.results} />
-          )}
-        </ErrorBoundary>
-      </section>
-    );
+  function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setCharName(value);
+    localStorage.setItem('inputName', value);
   }
+
+  return (
+    <section className="section-search">
+      <h2>Search</h2>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={charName}
+          onChange={handleNameChange}
+          className="search-input"
+        />
+
+        <Button onClick={sendRequest}>Search</Button>
+      </form>
+      <h2>Results</h2>
+      {error && <p className="error-message">{error}</p>}
+      <ErrorBoundary>
+        {loading ? <Spinner /> : <SearchResult results={results} />}
+      </ErrorBoundary>
+    </section>
+  );
 }
