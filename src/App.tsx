3dfe441ba52ab574ce027c8c25bb281f631dbd26
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-24 21:29:23 +0300

    feat: add About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Search } from './components/Search/Search';
+import { About } from './components/About/About';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Search />
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </main>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import logo from '/logo.png';
 import './Header.css';
+import { Link } from 'react-router';
 
 export function Header() {
   return (
     <header>
       <img className="header-logo" src={logo} alt="Rick and Morty" />
+      <Link to="/about">About</Link>
       <h1>Rick and Morty character search</h1>
     </header>
   );
