7a513d36aeea712de72a84c3259084e087ba2996
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-07-25 21:48:30 +0300

    feat: add tests About component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,36 @@
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('App', () => {
   it('renders App component', () => {
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>;
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('heading', { name: /rick and morty/i })
+    ).toBeInTheDocument();
+  });
+
+  it('navigates from About to Home page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', { name: /home/i });
+    await userEvent.click(button);
+
+    const title = await screen.findByRole('heading', {
+      name: /search/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
   });
 });
