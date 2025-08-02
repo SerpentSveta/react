86e0e269c97f850d69c66956bfd4c2a5ea872bef
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-08-02 22:42:29 +0300

    feat: add TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './normalize.css';
 import './index.css';
 import App from './App.tsx';
 
 const root = document.getElementById('root');
+const queryClient = new QueryClient();
 
 if (!root) {
   throw new Error('Root not found');
@@ -13,7 +15,9 @@ if (!root) {
   createRoot(root).render(
     <StrictMode>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </StrictMode>
   );
