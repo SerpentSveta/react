90dc6fc4a0a812eb7c90f3d36175a5ad59800f39
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-08-05 22:47:46 +0300

    fix: change App.test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,14 +2,35 @@ import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from '../App';
+import { useCharactersQuery } from '../query/useCharactersQuery';
+
+jest.mock('../query/useCharactersQuery', () => ({
+  useCharactersQuery: jest.fn(),
+}));
 
 describe('App', () => {
+  const queryClient = new QueryClient();
+
+  beforeEach(() => {
+    (useCharactersQuery as jest.Mock).mockReturnValue({
+      data: {
+        results: [],
+        info: { pages: 1 },
+      },
+      isPending: false,
+      error: null,
+    });
+  });
+
   it('renders App component', () => {
     render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </QueryClientProvider>
     );
 
     expect(
@@ -19,9 +40,11 @@ describe('App', () => {
 
   it('navigates from About to Home page', async () => {
     render(
-      <MemoryRouter initialEntries={['/about']}>
-        <App />
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      </QueryClientProvider>
     );
 
     const button = screen.getByRole('button', { name: /home/i });
@@ -36,9 +59,11 @@ describe('App', () => {
 
   it('switch theme', async () => {
     render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </QueryClientProvider>
     );
 
     const button = screen.getByRole('button', { name: /theme/i });
